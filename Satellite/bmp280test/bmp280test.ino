99e7d57691f19762855fdf8f3ef43e777bd5757a
Omar Roldan <omar.roldan.50@gmail.com>
2020-01-23 13:26:56 -0600

    BMP280 ino

--- Satellite/bmp280test/bmp280test.ino
+++ Satellite/bmp280test/bmp280test.ino
@@ -30,34 +30,35 @@ Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
+  //Serial.println(F("BMP280 test"));
 
   if (!bmp.begin()) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    //Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
 
   /* Default settings from datasheet. */
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+                  Adafruit_BMP280::SAMPLING_X8,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16);      /* Filtering. */
+                  //Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  //Serial.print(2000);
 }
 
 void loop() {
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
+    //Serial.print(F("Temperature = "));
+    //Serial.println(bmp.readTemperature());
+    //Serial.println(" *C");
 
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+    //Serial.print(F("Pressure = "));
+    //Serial.println(bmp.readPressure());
+    //Serial.println(" Pa");
 
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-    Serial.println(" m");
+    //Serial.print(F("Approx altitude = "));
+    Serial.println(bmp.readAltitude(1013.25)-2175); /* Adjusted to local forecast! */
+    //Serial.println(" m");
 
     Serial.println();
-    delay(200);
+    delay(30);
 }
